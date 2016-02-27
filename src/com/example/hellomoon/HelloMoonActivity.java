9283c31e6da7d829add2e3963752437eef337d83
ilya <MAAFI4I@gmail.com>
2016-02-27 21:05:07 +0200

    Activity HelloMoonActivity set as host activity for the fragment HelloMoonFragment

--- src/com/example/hellomoon/HelloMoonActivity.java
+++ src/com/example/hellomoon/HelloMoonActivity.java
@@ -1,9 +1,12 @@
 package com.example.hellomoon;
 
-import android.app.Activity;
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
-public class HelloMoonActivity extends Activity {
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+public class HelloMoonActivity extends FragmentActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
