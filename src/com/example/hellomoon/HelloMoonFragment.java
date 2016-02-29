492b4b23a9a6498cc77f8ceb1f05f6ada0227b47
ilya <MAAFI4I@gmail.com>
2016-02-29 21:14:54 +0200

    First decision

--- src/com/example/hellomoon/HelloMoonFragment.java
+++ src/com/example/hellomoon/HelloMoonFragment.java
@@ -1,56 +1,35 @@
 package com.example.hellomoon;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class HelloMoonFragment extends Fragment {
-	private Button mPlayButton;
-	private Button mStopButton;
-	private Button mPauseButton;
-	private AudioPlayer mPlayer = new AudioPlayer();
+	public static final String TAG ="HelloMoonFragment";
+	private VideoView mVideoView;
+	private MediaController mMediaController;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.fragment_hello_moon, parent, false);
 		
-		mPlayButton = (Button) v.findViewById(R.id.hellomoon_playButton);
-		mPlayButton.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				mPlayer.play(getContext());
-			}
-		});
-		
-		mStopButton = (Button) v.findViewById(R.id.hellomoon_stopButton);
-		mStopButton.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				mPlayer.stop();
-				
-			}
-		});
-		
-		mPauseButton = (Button) v.findViewById(R.id.hellomoon_pauseButton);
-		mPauseButton.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				mPlayer.pause();
-			}
-		});
-		
+		mVideoView = (VideoView) v.findViewById(R.id.hellomoon_videoView);
+		Uri videoUri = Uri.parse("android.resource://"+getContext().getPackageName()+"/"+R.raw.past_ense_song);
+		Log.d(TAG, videoUri.toString());
+		mVideoView.setVideoURI(videoUri);
+		mMediaController = new MediaController(getContext());
+		mVideoView.setMediaController(mMediaController);
 		return v;
 	}
 	
 	@Override 
 	public void onDestroy() {
 		super.onDestroy();
-		mPlayer.release();
 	}
 }
